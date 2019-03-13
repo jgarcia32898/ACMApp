7bca571cae036a974ad8f67aa354dc03ff891fdd
jgarcia32898 <jgarcia32898@gmail.com>
2019-03-12 17:34:35 -0700

    first commit

---
+++ src/main/java/Hackathons.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Hackathons {
+
+    @SerializedName("array")
+    List<Event> events;
+
+    public List<Event> getEvents(){
+        return events;
+    }
+
+
+}
